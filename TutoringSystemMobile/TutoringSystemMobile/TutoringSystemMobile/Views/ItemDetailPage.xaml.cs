64935ebac431d20b7e4de1f307e2a82a6fc87921
Marek Sroczkowski <marekes97@gmail.com>
2021-10-20 17:46:22 +0200

    Created new xamarin project with flyout navigation

---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using TutoringSystemMobile.ViewModels;
+using Xamarin.Forms;
+
+namespace TutoringSystemMobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TutoringSystemMobile.Models;
+using TutoringSystemMobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TutoringSystemMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
