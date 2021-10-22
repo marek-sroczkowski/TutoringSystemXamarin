42d0408ff02ff8e5faf04f8420e54aad181fa67c
Marek Sroczkowski <marekes97@gmail.com>
2021-10-22 15:02:31 +0200

    Added flyout navigation for tutor layout

--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/AppShell.xaml.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/AppShell.xaml.cs
@@ -11,8 +11,12 @@ namespace TutoringSystemMobile
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(StartTutorPage), typeof(StartTutorPage));
+            Routing.RegisterRoute(nameof(ReservationTutorPage), typeof(ReservationTutorPage));
+            Routing.RegisterRoute(nameof(OrdersTutorPage), typeof(OrdersTutorPage));
+            Routing.RegisterRoute(nameof(ReportTutorPage), typeof(ReportTutorPage));
+            Routing.RegisterRoute(nameof(StudentsTutorPage), typeof(StudentsTutorPage));
+            Routing.RegisterRoute(nameof(AccountTutorPage), typeof(AccountTutorPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
