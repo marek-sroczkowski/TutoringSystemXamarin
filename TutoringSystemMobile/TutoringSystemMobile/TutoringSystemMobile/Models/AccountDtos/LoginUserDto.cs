1af6023c5e16d770259ac1ece5376a4ad1b13a3a
Marek Sroczkowski <marekes97@gmail.com>
2021-10-24 16:07:04 +0200

    Added contructors for login dto

--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Models/AccountDtos/LoginUserDto.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Models/AccountDtos/LoginUserDto.cs
@@ -4,5 +4,15 @@
     {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public LoginUserDto()
+        {
+        }
+
+        public LoginUserDto(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
     }
 }
