1527a57395eda6665feb572764d78e4de2b34297
Marek Sroczkowski <marekes97@gmail.com>
2021-11-05 16:57:23 +0100

    Added extension to string returning new object id based on location url

---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace TutoringSystemMobile.Extensions
+{
+    public static class StringExtensions
+    {
+        public static long GetIdByLocation(this string location)
+        {
+            var locationItems = location?.Split('/');
+            if (locationItems is null || locationItems.Length == 0)
+                return -1;
+
+            var id = locationItems.LastOrDefault();
+
+            if (!long.TryParse(id, out long result))
+                return -1;
+
+            return result;
+        }
+    }
+}
