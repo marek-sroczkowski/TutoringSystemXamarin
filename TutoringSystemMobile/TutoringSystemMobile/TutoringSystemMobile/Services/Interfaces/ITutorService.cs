9a3a926c09132b1e6f5928e18b0de40db092d947
Marek Sroczkowski <marekes97@gmail.com>
2021-11-12 17:05:37 +0100

    Added support for tutor-related endpoints

--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Services/Interfaces/ITutorService.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Services/Interfaces/ITutorService.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using TutoringSystemMobile.Models.StudentDtos;
 using TutoringSystemMobile.Models.TutorDtos;
 
 namespace TutoringSystemMobile.Services.Interfaces
 {
     public interface ITutorService
     {
-        Task<bool> AddStudentAsync(long studentId);
-        Task<ICollection<StudentDto>> GetStudentsAsync();
-        Task<TutorDetailsDto> GetTutorAsync(long tutorId);
-        Task<bool> RemoveAllStudentsAsync();
-        Task<bool> RemoveStudentAsync(long studentId);
+        Task<IEnumerable<TutorDto>> GetTutorsAsync();
+        Task<TutorDetailsDto> GetTutorByIdAsync(long tutorId);
+        Task<bool> RemoveAllTutorsAsync();
+        Task<bool> RemoveTutorAsync(long tutorId);
+        Task<bool> AddTutorToStudentAsync(long tutorId);
     }
 }
--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Services/Web/TutorService.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/Services/Web/TutorService.cs
@@ -1,38 +1,82 @@
-﻿using System;
+﻿using Flurl;
+using Flurl.Http;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using TutoringSystemMobile.Models.StudentDtos;
 using TutoringSystemMobile.Models.TutorDtos;
 using TutoringSystemMobile.Services.Interfaces;
+using TutoringSystemMobile.Services.Web;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
+[assembly: Dependency(typeof(TutorService))]
 namespace TutoringSystemMobile.Services.Web
 {
     public class TutorService : ITutorService
     {
-        public Task<bool> AddStudentAsync(long studentId)
+        private readonly string baseUrl;
+
+        public TutorService()
         {
-            throw new NotImplementedException();
+            baseUrl = AppSettingsManager.Settings["BaseApiUrl"] + "tutor";
         }
 
-        public Task<ICollection<StudentDto>> GetStudentsAsync()
+        public async Task<bool> AddTutorToStudentAsync(long tutorId)
         {
-            throw new NotImplementedException();
+            string token = await SecureStorage.GetAsync("token");
+            var response = await baseUrl
+                .AppendPathSegment(tutorId)
+                .WithOAuthBearerToken(token)
+                .AllowAnyHttpStatus()
+                .PostAsync();
+
+            return response.StatusCode == 200;
         }
 
-        public Task<TutorDetailsDto> GetTutorAsync(long tutorId)
+        public async Task<IEnumerable<TutorDto>> GetTutorsAsync()
         {
-            throw new NotImplementedException();
+            string token = await SecureStorage.GetAsync("token");
+            var response = await baseUrl
+                .AllowAnyHttpStatus()
+                .WithOAuthBearerToken(token)
+                .GetAsync();
+
+            return response.StatusCode == 200 ? await response.GetJsonAsync<IEnumerable<TutorDto>>() : new List<TutorDto>();
         }
 
-        public Task<bool> RemoveAllStudentsAsync()
+        public async Task<TutorDetailsDto> GetTutorByIdAsync(long tutorId)
         {
-            throw new NotImplementedException();
+            string token = await SecureStorage.GetAsync("token");
+            var response = await baseUrl
+                .AllowAnyHttpStatus()
+                .AppendPathSegment(tutorId)
+                .WithOAuthBearerToken(token)
+                .GetAsync();
+
+            return response.StatusCode == 200 ? await response.GetJsonAsync<TutorDetailsDto>() : new TutorDetailsDto();
         }
 
-        public Task<bool> RemoveStudentAsync(long studentId)
+        public async Task<bool> RemoveAllTutorsAsync()
         {
-            throw new NotImplementedException();
+            string token = await SecureStorage.GetAsync("token");
+            var response = await baseUrl
+                .AllowAnyHttpStatus()
+                .AppendPathSegment("all")
+                .WithOAuthBearerToken(token)
+                .DeleteAsync();
+
+            return response.StatusCode == 204;
+        }
+
+        public async Task<bool> RemoveTutorAsync(long tutorId)
+        {
+            string token = await SecureStorage.GetAsync("token");
+            var response = await baseUrl
+                .AllowAnyHttpStatus()
+                .AppendPathSegment(tutorId)
+                .WithOAuthBearerToken(token)
+                .DeleteAsync();
+
+            return response.StatusCode == 204;
         }
     }
 }
