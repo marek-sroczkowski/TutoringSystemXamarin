1e7a099de1f6d44c4f390c66ac95c1862e35cc98
Marek Sroczkowski <marekes97@gmail.com>
2021-11-14 19:07:54 +0100

    Added nuget for popups

--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile.Android/MainActivity.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile.Android/MainActivity.cs
@@ -19,6 +19,8 @@ namespace TutoringSystemMobile.Droid
         {
             base.OnCreate(savedInstanceState);
 
+            Rg.Plugins.Popup.Popup.Init(this);
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
@@ -28,6 +30,11 @@ namespace TutoringSystemMobile.Droid
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public override void OnBackPressed()
+        {
+            Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed);
         }
     }
 
