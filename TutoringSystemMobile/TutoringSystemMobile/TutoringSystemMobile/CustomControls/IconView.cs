945d07492577381ac8d0064e1bd8239f446f4bd7
Marek Sroczkowski <marekes97@gmail.com>
2021-11-16 01:23:07 +0100

    Added outline for "My Profile" tab

---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/CustomControls/IconView.cs
@@ -0,0 +1,23 @@
+﻿using Xamarin.Forms;
+
+namespace TutoringSystemMobile.CustomControls
+{
+    public class IconView : View
+    {
+        public static readonly BindableProperty ForegroundProperty = BindableProperty.Create(nameof(Foreground), typeof(Color), typeof(IconView), default(Color));
+
+        public Color Foreground
+        {
+            get => (Color)GetValue(ForegroundProperty);
+            set => SetValue(ForegroundProperty, value);
+        }
+
+        public static readonly BindableProperty SourceProperty = BindableProperty.Create(nameof(Source), typeof(string), typeof(IconView), default(string));
+
+        public string Source
+        {
+            get => (string)GetValue(SourceProperty);
+            set => SetValue(SourceProperty, value);
+        }
+    }
+}
---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile/ViewModels/TutorProfileViewModels/TutorProfileViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TutoringSystemMobile.ViewModels.TutorProfileViewModels
+{
+    public class TutorProfileViewModel : BaseViewModel
+    {
+        public Command EditGeneralInformationsCommand { get; }
+        public Command EditAddressCommand { get; }
+        public Command EditContactCommand { get; }
+        public Command EditProfilePictureCommand { get; }
+        public Command ChangePasswordCommand { get; }
+        public Command DeactivateAccountCommand { get; }
+        public Command DarkModeCommand { get; }
+        public Command RateAppCommand { get; }
+        public Command LogoutCommand { get; }
+
+        public TutorProfileViewModel()
+        {
+            EditGeneralInformationsCommand = new Command(OnEditGeneralInformations);
+            EditAddressCommand = new Command(OnEditAddress);
+            EditContactCommand = new Command(OnEditContact);
+            EditProfilePictureCommand = new Command(OnEditProfilePicture);
+            ChangePasswordCommand = new Command(OnChangePassword);
+            DeactivateAccountCommand = new Command(OnDeactivateAccount);
+            DarkModeCommand = new Command(OnDarkMode);
+            RateAppCommand = new Command(OnRateApp);
+            LogoutCommand = new Command(OnLogout);
+        }
+
+        private void OnEditGeneralInformations()
+        {
+
+        }
+
+        private void OnEditAddress()
+        {
+
+        }
+
+        private void OnEditContact()
+        {
+
+        }
+
+        private void OnEditProfilePicture()
+        {
+
+        }
+
+        private void OnChangePassword()
+        {
+
+        }
+
+        private void OnDeactivateAccount()
+        {
+
+        }
+
+        private void OnDarkMode()
+        {
+
+        }
+
+        private void OnRateApp()
+        {
+
+        }
+
+        private void OnLogout()
+        {
+
+        }
+    }
+}
