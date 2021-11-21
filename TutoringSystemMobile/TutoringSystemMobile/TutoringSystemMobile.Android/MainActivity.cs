caae3406eb31476b70b37190f2ff0522aa51eef9
Marek Sroczkowski <marekes97@gmail.com>
2021-11-21 17:58:28 +0100

    Added new app icon and splash screen

--- TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile.Android/MainActivity.cs
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using ImageCircle.Forms.Plugin.Droid;
 
 namespace TutoringSystemMobile.Droid
 {
-    [Activity(Label = "TutoringSystemMobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "TutoringSystemMobile", Icon = "@drawable/logo_main", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ TutoringSystemMobile/TutoringSystemMobile/TutoringSystemMobile.Android/SplashActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.Animation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Com.Airbnb.Lottie;
+using System;
+
+namespace TutoringSystemMobile.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : Activity, Animator.IAnimatorListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ActivitySplashLayout);
+
+            var animationView = FindViewById<LottieAnimationView>(Resource.Id.animation_view);
+            animationView.AddAnimatorListener(this);
+        }
+
+        public void OnAnimationCancel(Animator animation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnAnimationEnd(Animator animation)
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public void OnAnimationRepeat(Animator animation)
+        {
+        }
+
+        public void OnAnimationStart(Animator animation)
+        {
+        }
+    }
+}
